2b6501b4f24e91a108e55374156b4d58da2c853b
s-hata <shinichi.hatano@ctc-g.co.jp>
2017-09-26 22:40:16 +0900

    update karm.conf.js file

--- karma.conf.js
+++ karma.conf.js
@@ -10,7 +10,7 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-junit-reporter'),
       require('karma-remap-istanbul'),
-      require('angular-cli/plugins/karma')
+      require('@angular/cli/plugins/karma')
     ],
     files: [
       { pattern: './src/test.ts', watched: false }
